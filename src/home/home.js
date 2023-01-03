33b0e200bb9731260839ef0a3703576339ecd14f
noorbhtiti <nour.bahtite@gmail.com>
2023-01-03 19:27:04 +0100

    Search box removed
    it is not in the requirements so we skip it and put some text instead

--- src/home/home.js
+++ src/home/home.js
@@ -1,15 +1,41 @@
 import './home-style.css'
 import Container from 'react-bootstrap/Navbar';
-import SearchBox from '../searchBox/searchBox';
+import React, { useState, useEffect } from "react";
+import Badge from 'react-bootstrap/Badge';
+
 
 
 
 function Home() {
+    let [text, setText] = useState("");
+    let words = ["Find course solutions", "Find course assignments", "Find course exams"];
+    let counter = 0;
+
+    useEffect(() => {
+        setText('sourced from students')
+        let interval = setInterval(() => {
+            setText(words[counter]);
+            counter = (counter + 1) % words.length;
+        }, 5000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <>
             <Container className="main-div">
                 <Container className='center-div'>
-                    <SearchBox />
+                    <h1 style={{
+                        "font-size": "7vh"
+
+                    }}>
+                        TentaP...
+                        <br />
+
+                        {text}
+                    </h1>
+
+
+
                 </Container>
             </Container>
         </>
